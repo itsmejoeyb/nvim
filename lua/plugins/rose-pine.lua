9010d09701df02afa9ced5fec4223a5014534a61
itsmejoeyb <31454088+itsmejoeyb@users.noreply.github.com>
2024-04-06 19:47:34 -0400

    changed keybind for toggleterm

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,19 +1,33 @@
--- return {
---   "rose-pine/neovim",
---   name = "rose-pine",
---   opts = {},
---   config = function()
---     vim.cmd([[ colorscheme rose-pine ]])
---   end,
--- }
+function RemoveBg(color)
+  color = color or "rose-pine"
+  vim.cmd.colorscheme(color)
+
+  -- set vim background to transparent
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
 
 return {
-  "tokyonight.nvim",
-  opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
+  {
+    "tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   opts = {},
+  --   config = function()
+  --     vim.cmd("colorscheme rose-pine")
+  --     require("rose-pine").setup({
+  --       disable_background = true,
+  --     })
+  --     RemoveBg()
+  --   end,
+  -- },
 }
